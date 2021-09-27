f2f52a21b8b27cc0901905afb9cb31f51f82e29c
yanyinuo <1601530253@qq.com>
2021-09-27 12:20:12 +0800

    react 脚手架配置代理的方式

--- 12-React脚手架/hello_r/src/components/setupProxy.js
+++ 12-React脚手架/hello_r/src/components/setupProxy.js
@@ -1,11 +1,20 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const createProxyMiddleware = require('http-proxy-middleware'); //可以把 createProxyMiddleware 写成proxy，名字无关
 
 module.export = function (app) {
     app.use(
-        '/api', //只有以 /api 开头的请求才能转发给 后端服务器
-        createProxyMiddleware({
-            target: 'http://localhost:5000', //转发目标
-            changeOrigin: true,
-        })
+        createProxyMiddleware(
+            '/api1', //只有以 /api 开头的请求才能转发给 后端服务器
+            {
+                target: 'http://localhost:5000', //转发目标
+                changeOrigin: true,//可以不要,控制服务器接收到的请求头中的HOST字段的值
+                pathRewrite: { '^/api1': '' }//路径重写，去掉前缀
+            }),
+        createProxyMiddleware(
+            '/api2',
+            {
+                target: 'http://localhost:5001',
+                changeOrigin: true,
+                pathRewrite: { '^/api2': '' }
+            })
     )
 }
